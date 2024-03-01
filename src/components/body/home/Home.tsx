88405fa3be0aa0f960dd235bff97ed75eeb9d360
João Victor <joaovictormendessilva1@gmail.com>
2024-03-01 13:02:37 -0300

    Adicionando e ajustando algumas animações de entrada e rolagem da página.

--- src/components/body/home/Home.tsx
+++ src/components/body/home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./Home.scss";
 
 export const Home = () => {
--- src/components/body/servicos/Servicos.tsx
+++ src/components/body/servicos/Servicos.tsx
@@ -1,13 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+
 import "./Servicos.scss";
 
 export const Servicos = () => {
+    const elementoRef = useRef<HTMLDivElement>(null);
+    const [elementoVisivel, setElementoVisivel] = useState<boolean>(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    setElementoVisivel(true);
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.4 });
+
+        if (elementoRef.current) {
+            observer.observe(elementoRef.current);
+        }
+
+        return () => {
+            if (elementoRef.current) {
+                observer.unobserve(elementoRef.current);
+            }
+        };
+    }, []);
+
     return (
         <div className="servicos">
             <h1 className="titulo extra-bold">Nossos Serviços:</h1>
 
             <div className="background-servicos">
 
-                <div className="fotos">
+                <div ref={elementoRef} className={ `fotos ${ elementoVisivel ? 'animado' : '' }` }>
                     <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/piso_vinilico.png?raw=true"
                         alt="Instalação de piso viniluco com textura de madeira escura para casas e apartamentos." className="foto-1" />
                     <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/piso_laminado.png?raw=true"
