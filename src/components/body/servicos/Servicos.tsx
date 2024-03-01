68660b9754917971e8c4e92e86a7a4688595113c
João Victor <joaovictormendessilva1@gmail.com>
2024-03-01 12:18:50 -0300

    Finalização da seção 'Nossos Serviços' e componente footer.

--- src/components/body/servicos/Servicos.tsx
+++ src/components/body/servicos/Servicos.tsx
@@ -2,8 +2,24 @@ import "./Servicos.scss";
 
 export const Servicos = () => {
     return (
-        <div className="background-servicos">
-            
+        <div className="servicos">
+            <h1 className="titulo extra-bold">Nossos Serviços:</h1>
+
+            <div className="background-servicos">
+
+                <div className="fotos">
+                    <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/piso_vinilico.png?raw=true"
+                        alt="Instalação de piso viniluco com textura de madeira escura para casas e apartamentos." className="foto-1" />
+                    <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/piso_laminado.png?raw=true"
+                        alt="Instalação de piso laminado com textura de madeira clara para casas e apartamentos." className="foto-2" />
+                    <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/rodape.png?raw=true"
+                        alt="Instalação de rodapé em poliestireno para casas e apartamentos." className="foto-3" />
+
+                    <div className="card">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,11 @@ import "./Footer.scss";
 function Footer() {
 
     return (
-        <h1>Footer</h1>
+        <div className="footer">
+            <h2 className="texto-footer descricao regular"> &copy; 
+                <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/nome_marca_fundo_transparente.png?raw=true" alt="Logo apêrfeito"/> 
+             - Todos os direitos reservados.</h2>
+        </div>
     )
 }
 
