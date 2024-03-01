e818f31eb037ee7f4bc3745e899b733a3441c14c
João Victor <joaovictormendessilva1@gmail.com>
2024-03-01 10:22:34 -0300

    New asset.

--- src/components/body/sobre/Sobre.tsx
+++ src/components/body/sobre/Sobre.tsx
@@ -3,8 +3,8 @@ import "./Sobre.scss";
 export const Sobre = () => {
     return (
         <div className="background-sobre">
-            <img src="" alt="" />
-
+            <div className="background-cor" />
+            
             <h1 className="titulo bold">Sobre Nós</h1>
             <h6 className="descricao regular">Somos uma empresa ...</h6>
         </div>
