ba9520681dabdf323e80e8f0f7bcceb446e417bb
João Victor <joaovictormendessilva1@gmail.com>
2024-03-06 07:10:22 -0300

    New asset.

--- src/components/whatsapp/WhatsApp.tsx
+++ src/components/whatsapp/WhatsApp.tsx
@@ -5,7 +5,7 @@ export function Whats() {
         <>
             <div className="whatsapp">
                 <a className="whatsapp" href="">
-                    <img className="whatsapp" src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/whatsapp.jpg?raw=true" 
+                    <img className="whatsapp" src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/logo_whatsapp.jpg?raw=true" 
                         alt="Entre em contato através do whatsapp da apêrfeito e solicite um orçamento para sua reforma ou construção." />
                 </a>
             </div>
