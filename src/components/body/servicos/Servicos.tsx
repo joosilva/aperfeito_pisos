87b740f8037f1f80c71a2eec74741605e069eaf4
João Victor <joaovictormendessilva1@gmail.com>
2024-03-06 06:28:18 -0300

    Finalização da seção de Serviços e início da seção de Sobre.

--- src/components/body/servicos/Servicos.tsx
+++ src/components/body/servicos/Servicos.tsx
@@ -4,7 +4,7 @@ import "./Servicos.scss";
 export const Servicos = () => {
     const elementoRef = useRef<HTMLDivElement>(null);
     const [elementoVisivel, setElementoVisivel] = useState<boolean>(false);
-    const [cardAberto, setCardAberto] = useState<number | null>(null); // Alterado: armazena o índice da imagem clicada
+    const [cardAberto, setCardAberto] = useState<number | null>(null);
 
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
@@ -27,17 +27,17 @@ export const Servicos = () => {
         };
     }, []);
 
-    const handleAbrirCard = (indice: number) => { // Alterado: recebe o índice da imagem clicada
-        setCardAberto(indice); // Alterado: armazena o índice da imagem clicada
+    const handleAbrirCard = (indice: number) => {
+        setCardAberto(indice);
     };
 
     const handleFecharCard = () => {
-        setCardAberto(null); // Alterado: fecha o card ao clicar no "X"
+        setCardAberto(null);
     };
 
     const handleFecharCardFora = (event: MouseEvent) => {
         if (elementoRef.current && !elementoRef.current.contains(event.target as Node)) {
-            setCardAberto(null); // Alterado: fecha o card ao clicar fora do card
+            setCardAberto(null);
         }
     };
 
@@ -55,19 +55,27 @@ export const Servicos = () => {
             <div className="background-servicos">
 
                 <div ref={elementoRef} className={`fotos ${elementoVisivel ? 'animado' : ''}`}>
-                    {['vinilico', 'laminado', 'boieserie', 'rodape'].map((servico, index) => ( // Alterado: adicionado um array de serviços para mapear
+                    {['vinilico', 'laminado', 'boieserie', 'rodape'].map((servico, index) => (
                         <img
                             key={index}
-                            onClick={() => handleAbrirCard(index)} // Alterado: passa o índice da imagem clicada para o manipulador de abertura de card
-                            src={`https://github.com/joosilva/aperfeito_pisos/blob/main/assets/${servico}.png?raw=true`} // Alterado: usa o índice para definir a imagem
-                            alt={`Instalação de piso ${servico}`} // Alterado: usa o índice para definir o texto alternativo
-                            className={`foto-${index + 1}`} // Alterado: adiciona o índice para a classe da imagem
+                            onClick={() => handleAbrirCard(index)}
+                            src={`https://github.com/joosilva/aperfeito_pisos/blob/main/assets/${servico}.png?raw=true`}
+                            alt={`Instalação de piso ${servico}`}
+                            className={`foto-${index + 1}`}
                         />
                     ))}
-                    {cardAberto !== null && ( // Alterado: exibe apenas o card correspondente à imagem clicada
-                        <div className={`card-${cardAberto === 0 ? 'vinilico' : cardAberto === 1 ? 'laminado' : cardAberto === 2 ? 'boieserie' : 'rodape'}`}>
-                            <button onClick={handleFecharCard} className="fechar-card">X</button>
-                            <p>Conteúdo do Card {cardAberto === 0 ? 'Vinílico' : cardAberto === 1 ? 'Laminado' : cardAberto === 2 ? 'Boieserie' : 'Rodapé'}</p>
+                    {cardAberto !== null && (
+                        <div className="card-overlay">
+                            <div className={`card-${cardAberto === 0 ? 'vinilico' : cardAberto === 1 ? 'laminado' : cardAberto === 2 ? 'boieserie' : 'rodape'}`}>
+                                <button onClick={handleFecharCard} className="fechar-card descricao extra-bold">X</button>
+                                <h1 className="titulo-card titulo bold">{cardAberto === 0 ? 'Piso Vinílico' : cardAberto === 1 ? 'Piso Laminado' : cardAberto === 2 ? 'Boieserie' : 'Rodapé em Poliestileno'}</h1>
+                                <p className="descricao regular">
+                                    {cardAberto === 0 ? 'O "Piso Vinílico" é ideal para interiores agregando elegancia e praticidade ao seu lar ou ambiente de trabalho. Sua composição é de PVC, o que permite a fácil limpeza do piso. Este revestimento possui uma fácil e rápida instalação por colagem, deixando o ambiente muito bem nivelado e trazendo mais praticidade e menos dor de cabeça na hora de realizar sua reforma.'
+                                        : cardAberto === 1 ? 'O "Piso Laminado" é ideal para interiores agregando elegancia e praticidade ao seu lar ou ambiente de trabalho. Sua composição é de HDF, o que permite a fácil limpeza do piso. Este revestimento possui uma fácil e rápida instalação por encaixe, trazendo muito mais praticidade e menos dor de cabeça na hora de realizar sua reforma.'
+                                            : cardAberto === 2 ? 'A "Boieserie" é uma peça que traz charme e classe a qualquer ambiente, sendo possível criar os mais diferentes tipos de texturas e molduras para as paredes ou teto do seu lar. Não se preocupe com a cor, pois este material pode receber tinta e você pode escolher a cor que desejar para a sua textura. Com certeza uma Boiserie traz muita leveza e aconchego para qualquer ambiente.'
+                                                : ' O "Rodapé em Poliestileno" é uma peça de muito versátil e proporciona um acabamento fino para a sua reforma. Este acabamento permite que você escolha a cor que mais combina e se adequa para cada ambiente e possui diferentes frisos, trazendo a sofisticação que você merece no seu lar ou ambiente de trabalho. Suas frestas internas possibilitam a passagem de rede elétrica, tornando possível dar vida ao seu projeto mais ousado, como colocar uma iluminação no rodapé, por exemplo.'}
+                                </p>
+                            </div>
                         </div>
                     )}
                 </div>
--- src/components/body/sobre/Sobre.tsx
+++ src/components/body/sobre/Sobre.tsx
@@ -6,7 +6,20 @@ export const Sobre = () => {
             <div className="background-cor" />
             
             <h1 className="titulo bold">Sobre Nós</h1>
-            <h6 className="descricao regular">Somos uma empresa...</h6>
+            <h6 className="descricao regular">
+                Somos uma empresa especializada em pisos e revestimentos com mais de 6 anos de mercado e centenas de clientes satsfeitos. <br />
+                Nossa principal preocupação é com nosso bem mais precioso, você. Tornar realidade o seu sonho para sua reforma é o nosso objetivo. <br />
+                Por isso, contamos com profissionais capacitados e especializados, oferecendo sempre o melhor do mercado para você.
+            </h6>
+            <h1 className="descricao bold">Nossos Parceiros:</h1>
+            <div className="parceiros">
+                <div className="linha-1">
+                    
+                </div>
+                <div className="linha-2">
+
+                </div>
+            </div>
         </div>
     )
 }
