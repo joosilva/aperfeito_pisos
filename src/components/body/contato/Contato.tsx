23bcb52e175a8c589338178eaec5abed5be289d1
João Victor <joaovictormendessilva1@gmail.com>
2024-03-06 18:34:22 -0300

    Adição de links para as redes sociais e new assets.

--- src/components/body/contato/Contato.tsx
+++ src/components/body/contato/Contato.tsx
@@ -10,17 +10,22 @@ export const Contato = () => {
 
 
             <button className="botao-1">
-                <a className="subtitulo regular" target="_blank"  href="https://wa.me/5511937370612?text=Ol%C3%A1.+Vim+pelo+site+e+gostaria+de+informa%C3%A7%C3%B5es%2Fsolicitar+um+or%C3%A7amento.">WhatsApp</a>
+                <a className="subtitulo regular" target="_blank" href="https://wa.me/5511937370612?text=Ol%C3%A1.+Vim+pelo+site+e+gostaria+de+informa%C3%A7%C3%B5es%2Fsolicitar+um+or%C3%A7amento.">WhatsApp</a>
                 <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/whatsapp_redondo.png?raw=true" alt="" />
             </button>
-            
+
             <button className="botao-2">
                 <a className="subtitulo regular" target="_blank" href="mailto:contato@aperfeitopisos.com.br?subject=Informa%C3%A7%C3%B5es/Or%C3%A7amento&body=Ol%C3%A1.+Vim+pelo+site+e+gostaria+de+informa%C3%A7%C3%B5es/solicitar+um+or%C3%A7amento.">Email</a>
                 <img className="email" src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/email.png?raw=true" alt="" />
             </button>
 
             <div className="redes">
-                
+                <a target="_blank" href="https://www.facebook.com/aperfeitopisos?mibextid=sCpJLy">
+                    <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/facebook.png?raw=true" alt="" />
+                </a>
+                <a target="_blank" href="https://www.instagram.com/aperfeitopisos/">
+                    <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/instagram.png?raw=true" alt="" />
+                </a>
             </div>
         </div>
     )
