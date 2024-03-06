b77ce53dd19beca928d1f41b0af20d89d0b35c18
João Victor <joaovictormendessilva1@gmail.com>
2024-03-06 08:04:10 -0300

    Finalização do componente 'Contato'.

--- src/components/body/contato/Contato.tsx
+++ src/components/body/contato/Contato.tsx
@@ -3,7 +3,17 @@ import "./Contato.scss";
 export const Contato = () => {
     return (
         <div className="contato">
-            
+            <h1 className="titulo bold">Entre em Contato</h1>
+            <h1 className="subtitulo bold">e solicite um orçamento gratuitamente:</h1>
+
+
+            <button>
+                <a className="subtitulo regular" href="">WhatsApp</a>
+                <img src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/whatsapp_redondo.png?raw=true" alt="" />
+            </button>
+
+            <h1 className="descricao regular">Aturalmente nosso contato é feito inteiramente pelo WhatsApp. <br />
+                Envie uma mensagem. Nossa equipe está pronta para te servir.</h1>
         </div>
     )
 }
--- src/components/whatsapp/WhatsApp.tsx
+++ src/components/whatsapp/WhatsApp.tsx
@@ -5,7 +5,7 @@ export function Whats() {
         <>
             <div className="whatsapp">
                 <a className="whatsapp" href="">
-                    <img className="whatsapp" src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/logo_whatsapp_transp.jpg?raw=true" 
+                    <img className="whatsapp" src="https://github.com/joosilva/aperfeito_pisos/blob/main/assets/whatsapp_redondo.png?raw=true" 
                         alt="Entre em contato através do whatsapp da apêrfeito e solicite um orçamento para sua reforma ou construção." />
                 </a>
             </div>
